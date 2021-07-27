7ead136b3609e08ee91e3df4881a705cf9f3a92f
volevol <i.fedorusov-2024@kpi.ua>
2021-07-27 16:41:05 +0300

    some changes again

--- prisma/migrations/20210722054621_added_default_level_exp/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "players" ALTER COLUMN "level" SET DEFAULT 0,
-ALTER COLUMN "experience" SET DEFAULT 0;
--- prisma/migrations/20210727121908_my_sch6/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "Order" ADD COLUMN     "address" TEXT NOT NULL DEFAULT E'none',
-ADD COLUMN     "delivery" TEXT NOT NULL DEFAULT E'pick up',
-ADD COLUMN     "id" SERIAL NOT NULL,
-ADD COLUMN     "payment" TEXT NOT NULL DEFAULT E'cash',
-ADD PRIMARY KEY ("id");
